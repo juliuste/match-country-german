9a97da50272073c9000730b3fd58afe81a8d2305
Julius Te <mail@juliuste.de>
2016-10-31 18:54:47 +0100

    fix for 'Gambia' and 'Sambia' :bug:

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const countries = require('./countries.json')
 const slug = require('slug')
 const levenshtein = require('fast-levenshtein').get
 
-const equals = (one, two) => (levenshtein(slug(one, ''), slug(two, '')) <= 1)
+const equals = (one, two) => (levenshtein(slug(one, ''), slug(two, '')) == 0) // levenshtein not necessary, a relict of an older version. used to tolerate distance 1, but since there is "gambia" vs "sambia" in german, this doesnt work out
 
 const match = (name) => {
 	for(let country in countries){
