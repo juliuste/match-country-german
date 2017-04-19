826c67630f78aed285609f24f505973a15e63ec6
Jannis R <mail@jannisr.de>
2017-04-19 13:01:03 +0200

    fast-levenshtein -> leven, slug -> normalize-for-search :zap:
    - leven seems to be faster than fast-levenshtein
    - normalize-for-search doesn't convert as many characters, but they don't
    seem to occur in this package
    
    before:
    
    exact match "Frankreich" x 557 ops/sec ±0.76% (88 runs sampled)
    fuzzy match "Großbritannein" x 149 ops/sec ±0.40% (79 runs sampled)
    too many typos "Großbrltannein" x 150 ops/sec ±1.07% (80 runs sampled)
    invalid input "Test" x 239 ops/sec ±0.93% (82 runs sampled)
    
    after:
    
    exact match "Frankreich" x 2,500 ops/sec ±0.62% (90 runs sampled)
    fuzzy match "Großbritannein" x 626 ops/sec ±0.66% (87 runs sampled)
    too many typos "Großbrltannein" x 632 ops/sec ±0.51% (87 runs sampled)
    invalid input "Test" x 1,156 ops/sec ±0.82% (87 runs sampled)

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 'use strict'
 
 const countries = require('./countries.json')
-const slug = require('slug')
-const levenshtein = require('fast-levenshtein').get
+const normalize = require('normalize-for-search')
+const levenshtein = require('leven')
 
-const equals = (one, two) => (levenshtein(slug(one, ''), slug(two, '')) == 0) // levenshtein not necessary, a relict of an older version. used to tolerate distance 1, but since there is "gambia" vs "sambia" in german, this doesnt work out
+const equals = (a, b) => {
+	return levenshtein(normalize(a), normalize(b)) === 0
+}
+// levenshtein not necessary, a relict of an older version. used to tolerate distance 1, but since there is "gambia" vs "sambia" in german, this doesnt work out
 
 const match = (name) => {
+
 	for(let country in countries){
 		if(equals(country, name)) return countries[country]
 	}
